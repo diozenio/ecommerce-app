2b2d8af17c64a039bac01452f56398b6f8b0c4d5
TzuChaeDahy <tzuchaedahy@gmail.com>
2025-10-21 21:39:34 -0300

    feat: add home screen, create UIInput and refactor some components

--- app/src/main/java/com/example/ecommerceapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/ecommerceapp/screens/HomeScreen.kt
@@ -1,15 +1,29 @@
 package com.example.ecommerceapp.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.ecommerceapp.data.cart.CartManager
+import com.example.ecommerceapp.ui.components.UIButton
 import com.example.ecommerceapp.ui.components.UIIcon
 import com.example.ecommerceapp.ui.components.UIIconName
+import com.example.ecommerceapp.ui.components.UIInput
+import com.example.ecommerceapp.ui.components.UIProductCard
 import com.example.ecommerceapp.ui.components.UIText
 import com.example.ecommerceapp.ui.components.UITextVariant
 import com.example.ecommerceapp.ui.components.UITextWeight
@@ -17,26 +31,73 @@ import com.example.ecommerceapp.ui.theme.Colors
 
 @Composable
 fun HomeScreen() {
+    val products = CartManager.products
+
+    var text by remember {
+        mutableStateOf("")
+    }
+
     Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
-        UIIcon(
-            icon = UIIconName.Home,
-            color = Colors.Primary900,
-            size = 48.dp
-        )
+        HomeHeader()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            UIInput(
+                text = text,
+                onChangeValue = { text = it },
+                placeholderText = "Search for clothes...",
+                leadingIcon = UIIconName.Search,
+                trailingIcon = UIIconName.Mic,
+                modifier = Modifier.weight(1f)
+            )
+            UIButton(
+                text = "",
+                leftIcon = {
+                    UIIcon(
+                        icon = UIIconName.Filter,
+                        color = Colors.Primary0
+                    )
+                },
+                fullWidth = false,
+            )
+        }
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            items(products, { it.id }) { product ->
+                UIProductCard(
+                    product = product,
+                    onUnsaveClick = {},
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun HomeHeader(modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
         UIText(
-            text = "Home Screen",
+            text = "Discover",
             variant = UITextVariant.H2,
             weight = UITextWeight.SemiBold
         )
-        UIText(
-            text = "Welcome to your ecommerce app!",
-            variant = UITextVariant.B3
+        UIIcon(
+            icon = UIIconName.Bell, modifier = Modifier.clickable { }
         )
     }
 }
---
+++ app/src/main/java/com/example/ecommerceapp/ui/components/UIProductCard.kt
@@ -0,0 +1,83 @@
+package com.example.ecommerceapp.ui.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.ecommerceapp.R
+import com.example.ecommerceapp.model.Product
+import com.example.ecommerceapp.ui.theme.Colors
+
+@SuppressLint("DefaultLocale")
+@Composable
+fun UIProductCard(product: Product, onUnsaveClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .clickable {}
+    ) {
+        Box {
+            Card(
+                shape = RoundedCornerShape(16.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            ) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(product.imageUrl)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = product.title,
+                    placeholder = painterResource(R.drawable.fallback_square),
+                    modifier = Modifier
+                        .aspectRatio(3f / 4f)
+                        .fillMaxWidth(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            IconButton(
+                onClick = onUnsaveClick,
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(8.dp)
+                    .background(Color.White, shape = RoundedCornerShape(size = 10.dp))
+            ) {
+                UIIcon(icon = UIIconName.HeartFilled)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        UIText(
+            text = product.title,
+            variant = UITextVariant.B2,
+            weight = UITextWeight.SemiBold
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        UIText(
+            text = "$ ${String.format("%.2f", product.price)}",
+            variant = UITextVariant.B3,
+            color = Colors.Primary600
+        )
+    }
+}
