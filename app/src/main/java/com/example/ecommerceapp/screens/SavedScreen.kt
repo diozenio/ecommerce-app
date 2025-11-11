8c0c13ea7c536aab9db86ac2e9f24cb9749f50d3
diozenio <d.dionisiobsl@gmail.com>
2025-11-10 21:47:08 -0300

    refactor: improve product card and saved screen UI
    Refactored `UIProductCard` to use Card component for better structure and added an `isSaved` state to toggle the heart icon.
    
    On the `SavedScreen`:
    - Replaced the custom `TopAppBar` with the new `UINavHeader` component.
    - Implemented the `UIEmptyState` component for when the saved items list is empty, improving the user experience.

--- app/src/main/java/com/example/ecommerceapp/screens/SavedScreen.kt
+++ app/src/main/java/com/example/ecommerceapp/screens/SavedScreen.kt
@@ -3,34 +3,25 @@ package com.example.ecommerceapp.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ecommerceapp.model.SavedViewModel
-import com.example.ecommerceapp.ui.components.UIIcon
+import com.example.ecommerceapp.ui.components.UIEmptyState
 import com.example.ecommerceapp.ui.components.UIIconName
+import com.example.ecommerceapp.ui.components.UINavHeader
 import com.example.ecommerceapp.ui.components.UIProductCard
-import com.example.ecommerceapp.ui.components.UIText
-import com.example.ecommerceapp.ui.components.UITextVariant
-import com.example.ecommerceapp.ui.components.UITextWeight
-import com.example.ecommerceapp.ui.theme.Colors
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,28 +30,12 @@ fun SavedScreen(viewModel: SavedViewModel = viewModel()) {
     val savedItems by viewModel.savedItems.collectAsState()
 
     Scaffold(
+        containerColor = Color.White,
         topBar = {
-            TopAppBar(
-                title = {
-                    UIText(
-                        text = "Saved Items",
-                        variant = UITextVariant.H2,
-                        weight = UITextWeight.SemiBold
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = { /* TODO: Handle back navigation */ }) {
-                        UIIcon(icon = UIIconName.Arrow)
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { /* TODO: Handle notification click */ }) {
-                        UIIcon(icon = UIIconName.Bell)
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.White
-                )
+            UINavHeader(
+                title = "Saved Items",
+                onBackPressed = { /*TODO*/ },
+                onNotificationPressed = { /*TODO*/ }
             )
         }
     ) { paddingValues ->
@@ -94,29 +69,11 @@ fun EmptySavedScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
-        UIIcon(
+        UIEmptyState(
             icon = UIIconName.HeartDuotone,
-            color = Colors.Primary300,
-            size = 48.dp
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-        UIText(
-            text = "No Saved Items",
-            variant = UITextVariant.H2,
-            weight = UITextWeight.SemiBold
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        UIText(
-            text = "You don't have any saved items.",
-            variant = UITextVariant.B3
-        )
-        UIText(
-            text = "Go to home and add some items to your saved list.",
-            variant = UITextVariant.B3
+            title = "Your Saved list Is Empty!",
+            description = "When you save products, they'll appear here."
         )
     }
 }
--- app/src/main/java/com/example/ecommerceapp/ui/components/UIProductCard.kt
+++ app/src/main/java/com/example/ecommerceapp/ui/components/UIProductCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,77 +27,89 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.ecommerceapp.R
+import com.example.ecommerceapp.model.Category
 import com.example.ecommerceapp.model.Product
 import com.example.ecommerceapp.model.UIProductCardCartSize
 import com.example.ecommerceapp.ui.theme.Colors
 
 @SuppressLint("DefaultLocale")
 @Composable
-fun UIProductCard(modifier: Modifier = Modifier, product: Product, onUnsaveClick: () -> Unit) {
-    Column(
+fun UIProductCard(
+    modifier: Modifier = Modifier,
+    product: Product,
+    onUnsaveClick: () -> Unit,
+    isSaved: Boolean = false
+) {
+    Card(
         modifier = Modifier
-            .clickable {}
+            .clickable {},
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
     ) {
-        Box {
-            Card(
-                shape = RoundedCornerShape(16.dp),
-            ) {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(product.imageUrl)
-                        .crossfade(true)
-                        .build(),
-                    contentDescription = product.title,
-                    placeholder = painterResource(R.drawable.fallback_square),
-                    error = painterResource(R.drawable.fallback_square),
-                    modifier = Modifier
-                        .aspectRatio(3f / 4f)
-                        .fillMaxWidth(),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            IconButton(
-                onClick = onUnsaveClick,
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .padding(8.dp)
-                    .background(Color.White, shape = RoundedCornerShape(size = 10.dp))
-            ) {
-                UIIcon(icon = UIIconName.HeartFilled)
+        Card(
+            modifier = Modifier.clickable {},
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White)
+        ) {
+            Column {
+                Box {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(product.imageUrl)
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = product.title,
+                        placeholder = painterResource(R.drawable.fallback_square),
+                        error = painterResource(R.drawable.fallback_square),
+                        modifier = Modifier
+                            .aspectRatio(3f / 4f)
+                            .fillMaxWidth(),
+                        contentScale = ContentScale.Crop
+                    )
+                    IconButton(
+                        onClick = onUnsaveClick,
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(8.dp)
+                            .background(Color.White, shape = RoundedCornerShape(size = 10.dp))
+                    ) {
+                        UIIcon(icon = if (isSaved) UIIconName.HeartFilled else UIIconName.Heart)
+                    }
+                }
             }
         }
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        UIText(
-            text = product.title,
-            variant = UITextVariant.B2,
-            weight = UITextWeight.SemiBold
-        )
+        Column(modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)) {
+            UIText(
+                text = product.title,
+                variant = UITextVariant.B2,
+                weight = UITextWeight.SemiBold
+            )
 
-        Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(4.dp))
 
-        UIText(
-            text = "$ ${String.format("%.2f", product.price)}",
-            variant = UITextVariant.B3,
-            color = Colors.Primary600
-        )
+            UIText(
+                text = "$ ${String.format("%.2f", product.price)}",
+                variant = UITextVariant.B3,
+                color = Colors.Primary600
+            )
+        }
     }
 }
 
 @Preview(showBackground = true, name = "Product Card Preview")
 @Composable
 fun UIProductCardPreview() {
-
     val sampleProduct = Product(
-        1,
-        "Camiseta de Algodão Premium Azul",
-        UIProductCardCartSize.MEDIUM,
-        12.5f,
-        "https://picsum.photos/id/1015/300/400",
-        1,
-        true
-
+        id = "id",
+        title = "Camiseta de Algodão Premium Azul",
+        size = UIProductCardCartSize.MEDIUM,
+        price = 12.5f,
+        imageUrl = "https://picsum.photos/id/1015/300/400",
+        discount = 12f,
+        category = Category(id = "id", title = "Camiseta")
     )
 
     Column(modifier = Modifier.padding(16.dp)) {
