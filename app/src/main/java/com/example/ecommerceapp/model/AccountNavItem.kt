4827e717fbbf634dd7c40cceb11cb4a60901cae9
TzuChaeDahy <tzuchaedahy@gmail.com>
2025-12-10 17:28:25 -0300

    fix: merge conflicts

--- app/src/main/java/com/example/ecommerceapp/model/AccountNavItem.kt
+++ app/src/main/java/com/example/ecommerceapp/model/AccountNavItem.kt
@@ -1,12 +1,5 @@
 package com.example.ecommerceapp.model
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import com.example.ecommerceapp.screens.AccountScreen
-import com.example.ecommerceapp.screens.CartScreen
-import com.example.ecommerceapp.screens.HomeScreen
-import com.example.ecommerceapp.screens.SavedScreen
-import com.example.ecommerceapp.screens.SearchScreen
 import com.example.ecommerceapp.ui.components.UIIconName
 
 sealed class AccountNavItem(
